a2db05afbaa7744f6d1347ea1dc76b64a3f646ca
João Lenon <lenonSec7@gmail.com>
2023-01-25 11:49:10 -0300

    fix(build): remove build folder and update gitignore

--- build/Constants/RealMethods.js
+++
@@ -1,5 +0,0 @@
-// src/Constants/RealMethods.ts
-var REAL_METHODS = /* @__PURE__ */ new Map();
-export {
-  REAL_METHODS
-};
--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { Ioc } from './Container/Ioc.js';
-export { Facade } from './Facades/Facade.js';
-export { ProviderFaker } from './Helpers/ProviderFaker.js';
-export { ServiceProvider } from './Providers/ServiceProvider.js';
-import 'awilix';
-import '#src/Container/Ioc';
-import '#src/Facades/FacadeProxyHandler';
