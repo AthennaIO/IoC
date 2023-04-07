3ac9d4b79c4b5f8e2224b9229adb3a9e18eee37e
João Lenon <lenonSec7@gmail.com>
2023-04-07 12:23:08 -0300

    chore(npm): update dependencies

--- src/Decorators/Inject.ts
+++ src/Decorators/Inject.ts
@@ -9,6 +9,8 @@
 
 import 'reflect-metadata'
 
+import { Facade } from '#src/index'
+
 /**
  * Inject some dependency of the service container in some property.
  */
@@ -18,50 +20,6 @@ export function Inject(alias?: string): PropertyDecorator {
       alias = key as string
     }
 
-    const dependency = ioc.use(alias)
-
-    if (dependency) {
-      defineValue(target, key, dependency)
-
-      return
-    }
-
-    defineValue(target, key, getProxyResolver(alias, dependency))
+    Object.defineProperty(target, key, { value: Facade.createFor(alias) })
   }
-}
-
-/**
- * Define some value in the target, values defined by Inject
- * decorator cannot be writable, enumerable or configurable.
- */
-function defineValue(target: string, key: string | symbol, value: any) {
-  Object.defineProperty(target, key, {
-    value,
-    writable: false,
-    enumerable: false,
-    configurable: false,
-  })
-}
-
-/**
- * Create a proxy that will resolve the dependency
- * when called.
- *
- * @example
- *  // If userService has not been resolved,
- *  // Proxy will resolve it and define the property.
- *  this.userService.findOne()
- */
-function getProxyResolver(alias: string, dependency: any) {
-  const handler = {
-    get: (_: any, key: string) => {
-      if (!dependency) {
-        dependency = ioc.safeUse(alias)
-      }
-
-      return dependency[key]
-    },
-  }
-
-  return new Proxy({}, handler)
 }
